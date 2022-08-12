fa8d8acf2e4a7c19b203e75ee175df25a8024eba
James Hortman <jamesdhortman@gmail.com>
2022-08-11 21:39:48 -0400

    small updates

--- src/main/java/com/flatiron/spring/SpringBookProject/model/Author.java
+++ src/main/java/com/flatiron/spring/SpringBookProject/model/Author.java
@@ -1,6 +1,8 @@
 package com.flatiron.spring.SpringBookProject.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,8 @@ import java.util.Set;
 @Setter
 @Table(name = "Author")
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
